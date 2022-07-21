60cc34df0f2d5f6973dd48b2b8655b5fb07f6e0e
sergey <ootieroo@gmail.com>
2022-07-21 22:29:55 +0300

    готово

---
+++ task01/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задача 41
+
+
+Console.Write($"Введите необхлдимое количество чисел: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] massiveNumbers = new int[m];
+
+void InputNumbers(int m){
+for (int i = 0; i < m; i++)
+  {
+    Console.Write($"Введите {i+1} число: ");
+    massiveNumbers[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+
+
+int Comparison(int[] massiveNumbers)
+{
+  int count = 0;
+  for (int i = 0; i < massiveNumbers.Length; i++)
+  {
+    if(massiveNumbers[i] > 0 ) count += 1; 
+  }
+  return count;
+}
+
+InputNumbers(m);
+
+Console.WriteLine($"Количество чисел больше 0: {Comparison(massiveNumbers)} ");
